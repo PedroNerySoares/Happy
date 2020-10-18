1e10b83d7d58be276f1f42ed61f49d9f9f64ecc3
Pedro Nery <pedrohnery@gmail.com>
2020-10-17 21:10:28 -0300

    second

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,3 @@
-import express from 'express'
-
-import './database/connection'
-
-
 //Rota = conjuto
 //Recurso= usuário[users]
 //Métodos= HTTP = GET,POST,PUT,DELETE
@@ -18,14 +13,43 @@ import './database/connection'
 //Route parasms: http://localhost:3333/users/1 (identificar um recurso)
 //Body: http://localhost:3333/users (identificar um recurso)
 
+import express from 'express';
+import{getRepository} from 'typeorm';
+import Orphanage from './models/Orphanages'
+
 const app=express()
 app.use(express.json())
 
 
-app.get('/users',(requeste,response)=>{
+app.post('/orphanages',async(request,response)=>{
+    const{
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        opening_weekends,
+
+    }=request.body;
+    
+    const orphanagesRepository= getRepository(Orphanage)
+    const orphanage = orphanagesRepository.create({
+            
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            opening_weekends,
+
+
+    });
+
+    await orphanagesRepository.save(orphanage);    
+   
     return response.json({message:'hello modafoca2'});
-
-
 
 })
 
